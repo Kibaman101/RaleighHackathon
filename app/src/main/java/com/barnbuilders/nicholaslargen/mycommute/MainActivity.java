6aacc92cdd203a3d9335a8085058c71ff510536f
jordanmather <jordanjmather@gmail.com>
2015-04-18 14:03:50 -0500

    started a dashboardButton method to start the DashboardActivity when the button is clicked

--- app/src/main/java/com/barnbuilders/nicholaslargen/mycommute/MainActivity.java
+++ app/src/main/java/com/barnbuilders/nicholaslargen/mycommute/MainActivity.java
@@ -1,9 +1,11 @@
 package com.barnbuilders.nicholaslargen.mycommute;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +37,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void dashboardButton()
+    {
+        //when this button is clicked, start the DashboardActivity
+        Intent intent = new Intent(this, DashboardActivity.class);
+        
     }
 }
